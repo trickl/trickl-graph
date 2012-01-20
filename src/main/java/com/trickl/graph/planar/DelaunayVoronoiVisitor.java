d7e292527ef2788b10e32fe33806ad2cf88348bb
tgee <tgee@seville>
2012-01-20 12:31:31 +0000

    Refactoring common code.

--- src/main/java/com/trickl/graph/planar/DelaunayVoronoiVisitor.java
+++ src/main/java/com/trickl/graph/planar/DelaunayVoronoiVisitor.java
@@ -21,28 +21,18 @@
 package com.trickl.graph.planar;
 
 import com.trickl.graph.edges.DirectedEdge;
+import com.vividsolutions.jts.algorithm.Angle;
 import com.vividsolutions.jts.algorithm.CGAlgorithms;
 import com.vividsolutions.jts.algorithm.ConvexHull;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.CoordinateList;
-import com.vividsolutions.jts.geom.CoordinateSequenceFactory;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineSegment;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.Triangle;
+import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequenceFactory;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import org.jgrapht.VertexFactory;
 
 public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1, E1, V2, E2> implements PlanarLayout<V2> {
 
    final private PlanarLayout<V1> inputLocations;
-   final private Map<V2, Coordinate> dualLocations;
+   final private Map<V2, Coordinate> vertexToCoordinate;
    final private LinearRing boundary;
    final private List<V2> boundaryVertices;
 
@@ -64,13 +54,13 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
          throw new NullPointerException();
       }
 
-      // TODO: Validate that dual graph boundary is convex
+      // Validate that dual graph boundary is convex
       if (!PlanarGraphs.isBoundaryConvex(inputGraph, inputLayout)) {
          throw new IllegalArgumentException("Delaunay graphs must have a convex boundary.");
       }
 
       this.inputLocations = inputLayout;
-      this.dualLocations = new HashMap<V2, Coordinate>();
+      this.vertexToCoordinate = new HashMap<V2, Coordinate>();
       this.boundary = boundary;
       this.boundaryVertices = new ArrayList<V2>(boundary == null ? 0
               : boundary.getCoordinates().length);
@@ -78,9 +68,19 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
       if (boundary != null) {
          // Note that in a linear ring, end element equals start element
          for (int i = 0; i < boundary.getCoordinates().length - 1; ++i) {
+            Coordinate coord = boundary.getCoordinateN(i);
+            Coordinate nextCoord = boundary.getCoordinateN(i + 1);
+            Coordinate nextNextCoord = boundary.getCoordinateN((i + 2) 
+                    % (boundary.getCoordinates().length - 1));
+            if (Angle.getTurn(Angle.angle(coord, nextCoord),
+                    Angle.angle(nextCoord, nextNextCoord))
+                    == Angle.COUNTERCLOCKWISE) {
+               throw new IllegalArgumentException("Boundary must be defined clockwise");
+            }
+
             V2 boundaryVertex = vertexFactory.createVertex();
             boundaryVertices.add(boundaryVertex);
-            dualLocations.put(boundaryVertex, boundary.getCoordinateN(i));
+            vertexToCoordinate.put(boundaryVertex, coord);
             this.dualGraph.addVertex(boundaryVertex);
          }
       }
@@ -104,13 +104,10 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
    public void endTraversal() {
       super.endTraversal();
 
-      // TODO: Handle cases where the half lines to the boundary intersect
-      // (currently causes an error about the before and after edges not
-      // sharing a face - which is true...).
-
+      // Assumes the boundary is convex so half lines should never intersect.
       // Join up the boundary vertices, note the boundary faces
       // outwards so we take care to create an inward face
-      
+
       Collections.reverse(boundaryVertices);
       for (int prevItr = 0; prevItr < boundaryVertices.size(); ++prevItr) {
          int itr = (prevItr + 1) % boundaryVertices.size();
@@ -129,7 +126,7 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
          E2 edge = edgeFactory.createEdge(boundarySource, boundaryTarget);
          dualGraph.addEdge(boundarySource, boundaryTarget, boundaryBefore, null, edge);
       }
-      
+
    }
 
    private void locateVertex(PlanarGraph<V1, E1> inputGraph,
@@ -137,7 +134,7 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
            V1 delaunayTarget,
            V2 voronoiSource,
            V2 voronoiTarget) {
-      if (!dualLocations.containsKey(voronoiSource)) {
+      if (!vertexToCoordinate.containsKey(voronoiSource)) {
 
          // Record the vertex before this boundary edge
          V2 voronoiBefore = dualGraph.containsEdge(voronoiTarget, voronoiSource)
@@ -168,105 +165,65 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
                  && CGAlgorithms.isPointInRing(circumcentre, boundary.getCoordinates())) {
             // Location is the cirumcenter of face, which is also the circumcenter of
             // any set of three points of the face                        
-            dualLocations.put(voronoiSource, circumcentre);
+            vertexToCoordinate.put(voronoiSource, circumcentre);
          } else {
             // Location is the intersection of the boundary and the perpendicular
-            // bisector of the delaunay edge            
-            V2 boundaryVertex = null;
+            // bisector of the delaunay edge                        
             Coordinate midPoint = ab.midPoint();
-            LineSegment perpendicularBisector = new LineSegment(
+            V2 boundaryVertex = createVertexAtBoundaryInterception(new LineSegment(
                     midPoint,
                     new Coordinate(midPoint.x - (b.y - a.y),
-                    midPoint.y - (a.x - b.x)));
-
-            // Check each segment in the boundary for intersection with the
-            // bisector            
-            double minDistance = Double.POSITIVE_INFINITY;
-            int segmentIndex = -1;
-            for (int itr = 0; itr < boundaryVertices.size(); ++itr) {
-               Coordinate intersection = getBoundaryIntersection(perpendicularBisector, itr);
-               if (intersection != null) {
-                  // Find the nearest boundary intersection
-                  double distance = midPoint.distance(intersection);
-                  if (distance < minDistance) {
-                     minDistance = distance;
-                     segmentIndex = itr;
-                  }
-               }
-            }
-
-            if (segmentIndex >= 0) {
-               int nextItr = (segmentIndex + 1) % boundaryVertices.size();
-               LineSegment boundarySegment = getBoundarySegment(segmentIndex);
-               Coordinate intersection = getBoundaryIntersection(perpendicularBisector, segmentIndex);
-
-               if (intersection.equals(boundarySegment.p0)) {
-                  boundaryVertex = boundaryVertices.get(segmentIndex);
-               } else if (intersection.equals(boundarySegment.p1)) {
-                  boundaryVertex = boundaryVertices.get(nextItr);
-               } else {
-                  boundaryVertex = vertexFactory.createVertex();
-                  dualLocations.put(boundaryVertex, intersection);
-                  boundaryVertices.add(nextItr, boundaryVertex);
-               }
-
-               // Create the new edge to this boundary intercept
-               //if (dualGraph.edgeSet().size() < 20) {
-                  E2 edge = edgeFactory.createEdge(voronoiTarget, boundaryVertex);
-                  dualGraph.addEdge(voronoiTarget, boundaryVertex, voronoiBefore, null, edge);
-                  getEdgeToVertexMap().put(new DirectedEdge<V1>(delaunaySource, delaunayTarget), boundaryVertex);
-               //}
+                    midPoint.y - (a.x - b.x))),
+                       boundaryVertices,
+                       vertexFactory,
+                       this);
+            if (boundaryVertex != null) {
+              // Create the new edge to this boundary intercept
+               E2 edge = edgeFactory.createEdge(voronoiTarget, boundaryVertex);
+               dualGraph.addEdge(voronoiTarget, boundaryVertex, voronoiBefore, null, edge);
+               getEdgeToVertexMap().put(new DirectedEdge<V1>(delaunaySource, delaunayTarget), boundaryVertex);
             }
          }
       }
    }
    
-   private LineSegment getBoundarySegment(int boundarySegmentIndex) {      
-      int nextItr = (boundarySegmentIndex + 1) % boundaryVertices.size();
-      V2 boundarySource = boundaryVertices.get(boundarySegmentIndex);
-      V2 boundaryTarget = boundaryVertices.get(nextItr);
-      return new LineSegment(
-              dualLocations.get(boundarySource),
-              dualLocations.get(boundaryTarget));
-   }
+   private V2 createVertexAtBoundaryInterception(LineSegment halfLine, List<V2> boundaryVertices, VertexFactory<V2> vertexFactory, PlanarLayout<V2> layout) {
+      V2 boundaryVertex = null;
+      int segmentIndex = PlanarGraphs.getNearestInterceptingLineSegment(halfLine, boundaryVertices, layout);      
+      if (segmentIndex >= 0) {
+         int nextItr = (segmentIndex + 1) % boundaryVertices.size();
+         LineSegment boundarySegment = PlanarGraphs.getLineSegment(segmentIndex, boundaryVertices, layout);
+         Coordinate intersection = PlanarGraphs.getHalfLineIntersection(halfLine, boundarySegment);
 
-   private Coordinate getBoundaryIntersection(LineSegment line, int boundarySegmentIndex) {
-      LineSegment boundarySegment = getBoundarySegment(boundarySegmentIndex);
+         if (intersection.equals(boundarySegment.p0)) {
+            boundaryVertex = boundaryVertices.get(segmentIndex);
+         } else if (intersection.equals(boundarySegment.p1)) {
+            boundaryVertex = boundaryVertices.get(nextItr);
+         } else {
+            boundaryVertex = vertexFactory.createVertex();
+            vertexToCoordinate.put(boundaryVertex, intersection);
+            boundaryVertices.add(nextItr, boundaryVertex);
+         }
+      }
 
-      // The bisector needs to be extended to reach the boundary,
-      // only project fowards (factor must be positive)
-      double factor = Math.max(0, Math.max(line.projectionFactor(boundarySegment.p0),
-              line.projectionFactor(boundarySegment.p1)));
-
-      LineSegment extendedBisector =
-              new LineSegment(line.p0, line.pointAlong(factor));
-
-      // Check for intersection with this boundary segment
-      Coordinate boundaryIntersection = extendedBisector.intersection(boundarySegment);
-      return boundaryIntersection;
+      return boundaryVertex;
    }
 
    @Override
    public Coordinate getCoordinate(V2 vertex) {
-      return dualLocations.get(vertex);
-
-
+      return vertexToCoordinate.get(vertex);
    }
 
    public static <V> List<V> getConvexBoundaryVertices(List<V> vertices, PlanarLayout<V> planarLayout) {
       Map<Coordinate, V> coordinateToVertex = new HashMap<Coordinate, V>();
       CoordinateList flattenedLocations = new CoordinateList();
 
-
       for (V vertex : vertices) {
          Coordinate location = planarLayout.getCoordinate(vertex);
-
 
          if (location != null) {
             flattenedLocations.add(location);
             coordinateToVertex.put(location, vertex);
-
-
          }
       }
 
@@ -276,7 +233,6 @@ public class DelaunayVoronoiVisitor<V1, E1, V2, E2> extends DualGraphVisitor<V1,
       Coordinate[] boundary = convexHull.getConvexHull().getCoordinates();
 
       List<V> boundaryVertices = new LinkedList<V>();
-
 
       for (int i = 0; i < (boundary.length - 1); ++i) {
          boundaryVertices.add(coordinateToVertex.get(boundary[i]));
