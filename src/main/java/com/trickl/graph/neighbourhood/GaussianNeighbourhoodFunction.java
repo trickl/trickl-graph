e262e003d3664a6b1f82fef750339b4ffcdceee8
Tim Gee <writetotimgee@gmail.com>
2016-07-07 21:18:11 +0100

    Upgraded apache commons math.

--- src/main/java/com/trickl/graph/neighbourhood/GaussianNeighbourhoodFunction.java
+++ src/main/java/com/trickl/graph/neighbourhood/GaussianNeighbourhoodFunction.java
@@ -21,17 +21,15 @@
 package com.trickl.graph.neighbourhood;
 
 import com.trickl.graph.NeighbourhoodFunction;
-import org.apache.commons.math.MathException;
-import org.apache.commons.math.distribution.NormalDistribution;
-import org.apache.commons.math.distribution.NormalDistributionImpl;
+import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class GaussianNeighbourhoodFunction implements NeighbourhoodFunction {
 
-   private NormalDistribution normalDistribution = new NormalDistributionImpl(0, 3);
+   private NormalDistribution normalDistribution = new NormalDistribution(0, 3);
    
    @Override
    public void setNeighbourhoodWidth(double neighbourhoodWidth) {
-      normalDistribution.setStandardDeviation(neighbourhoodWidth);
+      normalDistribution = new NormalDistribution(0, neighbourhoodWidth);
    }
 
    @Override
@@ -43,13 +41,10 @@ public class GaussianNeighbourhoodFunction implements NeighbourhoodFunction {
    public double evaluate(double distance) {
       double density = 0;
       double delta = 1e-5;
-      try {
          // Use a gaussian distribution such that P(0) = 1.
-         density = normalDistribution.cumulativeProbability(distance, distance + delta) /
-                   normalDistribution.cumulativeProbability(0, delta);
-      }
-      catch (MathException ex) {
-      }
+         density = normalDistribution.probability(distance, distance + delta) /
+                   normalDistribution.probability(0, delta);
+      
       return density;
    }
 }
